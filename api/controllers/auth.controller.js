84d7c26095e9eb56222cc1358a26204cfaa9835b
NIDHIN-LAL <lalnidhinp02@gmail.com>
2024-07-02 22:21:14 +0530

    created the signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utilities/error.js';
 
 export const signup = async (req, res, next) => {
   console.log(req.body);
@@ -11,6 +13,28 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return next(errorHandler(404, 'User not found'));
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if (!isMatch) return next(errorHandler(404, 'Invalid credentials'));
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = user._doc;
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,35 +1,76 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      setLoading(true);
+      e.preventDefault();
+      console.log(formData);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      navigate('/sign-in');
+      setError(null);
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className='p-5 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-      <form className='flex flex-col gap-3 '>
+      <form
+        onSubmit={handleSubmit}
+        className='flex flex-col gap-3 '>
         <input
           type='text'
           placeholder='Name'
           id='username'
+          onChange={handleChange}
           className='p-3 border border-slate-300 rounded-md outline-none'
         />
         <input
           type='email'
           placeholder='Email'
           id='email'
+          onChange={handleChange}
           className='p-3 border border-slate-300 rounded-md outline-none'
         />
         <input
           type='password'
           placeholder='Password'
           id='password'
+          onChange={handleChange}
           className='p-3 border border-slate-300 rounded-md outline-none'
         />
-        <input
-          type='password'
-          placeholder='Confirm Password'
-          className='p-3 border border-slate-300 rounded-md outline-none'
-        />
-        <button className='bg-slate-800 text-white p-3 rounded-md my-3 hover:opacity-95 disabled:opacity-80'>
-          Sign Up
+
+        <button
+          disabled={loading}
+          className='bg-slate-800 text-white p-3 rounded-md my-3 hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className='flex gap-2 mt-2'>
@@ -42,6 +83,8 @@ export default function SignUp() {
           </Link>
         </p>
       </div>
+
+      {error && <p className='text-red-500'>{error}</p>}
     </div>
   );
 }
