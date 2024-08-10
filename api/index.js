10f818b85e6a7ff30ed7c1ba1ce93cdf59b8ad0d
NidhinLal <nidhin.lal@foxena.com>
2024-08-10 23:48:44 +0530

    creating lising api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import * as dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 /*MONGO="mongodb+srv://lalnidhinp02:Nidhin%402020@cluster0.sa4gaet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -31,6 +32,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
